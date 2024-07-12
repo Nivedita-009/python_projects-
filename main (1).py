aad8af86451c7b30e5d8a9a807f09a9f9824cae9
Nivedita Dixit <anahitadixit@gmail.com>
2024-07-12 18:54:45 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,6 @@
+print("Welcome To The Band Name Generator")
+city=input("what's the name of the city you grew up in ?\n")
+
+pet=input("whats your pet name \n")
+
+print("your band name could be "+city+" "+pet)
